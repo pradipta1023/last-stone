9d50007e28ed381fdb90aa73f67d8d790aa11c3f
Pradipta Dey <pradiptadey@Pradiptas-MacBook-Pro.local>
2026-02-06 19:08:34 +0530

    Adds basic validations like move should be in range only valid moves should be played   * Connections are closed after a game ends

--- src/requestHandler.js
+++ src/requestHandler.js
@@ -6,14 +6,33 @@ const buffer = new Uint8Array(1024);
 
 const broadcast = async (players, message) => {
   for (const player of players) {
-    await player.conn.write(encoder.encode(message));
+    await write(player.conn, message);
   }
 };
 
+const isInRange = (value) => value >= 1 && value <= 3;
+
+const write = async (conn, message) =>
+  await conn.write(encoder.encode(message));
+
 const getUserInput = async (player) => {
+  await write(player.conn, "\nEnter your move \n> ");
   const bytesRead = await player.conn.read(buffer);
-  const userInput = +decoder.decode(buffer.slice(0, bytesRead));
-  return userInput;
+  const userInput = decoder.decode(buffer.slice(0, bytesRead));
+  const parsedInput = +userInput;
+
+  if (!Number.isInteger(parsedInput) || !isInRange(parsedInput)) {
+    await write(player.conn, `\n${userInput} is not a valid move\n`);
+    return getUserInput(player);
+  }
+
+  return parsedInput;
+};
+
+const closeConnections = async (players) => {
+  for (const player of players) {
+    await player.conn.close();
+  }
 };
 
 const handleConnection = async (players) => {
@@ -21,23 +40,23 @@ const handleConnection = async (players) => {
   let noOfSticks = 8;
   let turn = 0;
   while (noOfSticks > 0) {
-    const player = players[turn];
-    await player.conn.write(encoder.encode("\nEnter your move \n> "));
-    const userInput = await getUserInput(player);
+    const userInput = await getUserInput(players[turn]);
     turn = 1 - turn;
     noOfSticks -= userInput;
     await broadcast(players, `\nSticks left: ${noOfSticks}\n`);
   }
-  broadcast(players, `Winner: Player${(1 - turn) + 1}`);
+  await broadcast(players, `Winner: Player${(1 - turn) + 1}\n`);
+  await closeConnections(players);
 };
 
 export const listen = async (port) => {
   const listener = await init(port);
-  const players = [];
+  let players = [];
   for await (const conn of listener) {
     players.push({ conn });
     if (players.length === 2) {
       await handleConnection(players);
+      players = [];
     }
   }
 };
