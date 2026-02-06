af9114621f6a55d63088948b92a3f33e6651bd50
Pradipta Dey <pradiptadey@Pradiptas-MacBook-Pro.local>
2026-02-06 18:21:15 +0530

    HandleConnection function is now able to take userInputon the basis of turn

--- src/requestHandler.js
+++ src/requestHandler.js
@@ -1,6 +1,8 @@
 export const init = async (port) => await Deno.listen({ port });
 
 const encoder = new TextEncoder();
+const decoder = new TextDecoder();
+const buffer = new Uint8Array(1024);
 
 const broadcast = async (players) => {
   for (const player of players) {
@@ -8,8 +10,23 @@ const broadcast = async (players) => {
   }
 };
 
+const getUserInput = async (player) => {
+  const bytesRead = await player.conn.read(buffer);
+  const userInput = +decoder.decode(buffer.slice(0, bytesRead));
+  return userInput;
+};
+
 const handleConnection = async (players) => {
   await broadcast(players);
+  let noOfSticks = 8;
+  let turn = 0;
+  while (noOfSticks > 0) {
+    const player = players[turn];
+    await player.conn.write(encoder.encode("\nEnter your move \n> "));
+    const userInput = await getUserInput(player);
+    turn = 1 - turn;
+    noOfSticks -= userInput;
+  }
 };
 
 export const listen = async (port) => {
